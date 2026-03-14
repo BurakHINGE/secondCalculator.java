7635086d68cee8bbb078820b46a3f78d39379a8d
BurakHINGE <burakmentese16@gmail.com>
2026-03-14 03:33:16 +0300

    Update eng_SecondsCalculator.java
    Zero situations have been fixed.

--- eng_SecondsCalculator.java
+++ eng_SecondsCalculator.java
@@ -4,33 +4,33 @@ public class eng_SecondsCalculator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter the number of seconds: ");
-        int enteredSeconds = input.nextInt();
-    
-        int hours = enteredSeconds / 3600;
-        int remainingSecondsAfterHours = enteredSeconds % 3600;
-        int minutes = remainingSecondsAfterHours / 60;
-        int remainingSecondsAfterMinutes = remainingSecondsAfterHours % 60;
-        int remainingSeconds = remainingSecondsAfterMinutes;
+        while(true) {
 
-        if (hours < 1 && minutes < 1) {
-            System.out.println(enteredSeconds + " seconds is equal to " + remainingSeconds);
-        }
-        else if (hours < 1 && remainingSeconds < 1) {
-            System.out.println(enteredSeconds + " seconds is equal to " + minutes + " minutes.");
-        }
-        else if (minutes < 1 && remainingSeconds < 1) {
-            System.out.println(enteredSeconds + " seconds is equal to " + hours + " hours.");
-        }
-        else if (hours < 1) {
-            System.out.println(enteredSeconds + " seconds is equal to " + minutes + " minutes, and " + remainingSeconds + " seconds.");
-        }
-        else if (minutes < 1) {
-            System.out.println(enteredSeconds + " seconds is equal to " + hours + " hours, and "  + remainingSeconds + " seconds.");
-        }
-        else if (remainingSeconds < 1) {
-            System.out.println(enteredSeconds + " seconds is equal to " + hours + " hours, and " + minutes + " minutes.");
-        }
+            System.out.print("Enter the number of seconds: ");
+            int enteredSeconds = input.nextInt();
         
+            int hours = enteredSeconds / 3600;
+            int remainingSecondsAfterHours = enteredSeconds % 3600;
+            int minutes = remainingSecondsAfterHours / 60;
+            int remainingSecondsAfterMinutes = remainingSecondsAfterHours % 60;
+            int remainingSeconds = remainingSecondsAfterMinutes;
+
+            System.out.print((enteredSeconds == 1) ? enteredSeconds + " second is equal to: " : enteredSeconds + " seconds are equal to: ");
+
+            if (hours > 0) {
+                System.out.print(hours + " " + ((hours == 1) ? "hour" : "hours") + " ");
+            }
+            if (minutes > 0) {
+                System.out.print(minutes + " " + ((minutes == 1) ? "minute" : "minutes") + " ");
+            }
+            if (remainingSeconds > 0) {
+                System.out.print(remainingSeconds + " " + ((remainingSeconds == 1) ? "second" : "seconds") + " ");
+            }
+            if (enteredSeconds == 0) {
+                System.out.print(enteredSeconds + " seconds");
+            }
+
+            System.out.println();
+        }
     }   
 }
