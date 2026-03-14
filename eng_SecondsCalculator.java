005e4cf03517d8e385164104cd92cc7649c66bbc
BurakHINGE <burakmentese16@gmail.com>
2026-03-14 03:01:59 +0300

    Update eng_SecondsCalculator.java
    Output improvements.

--- eng_SecondsCalculator.java
+++ eng_SecondsCalculator.java
@@ -12,7 +12,25 @@ public class eng_SecondsCalculator {
         int minutes = remainingSecondsAfterHours / 60;
         int remainingSecondsAfterMinutes = remainingSecondsAfterHours % 60;
         int remainingSeconds = remainingSecondsAfterMinutes;
+
+        if (hours < 1 && minutes < 1) {
+            System.out.println(enteredSeconds + " seconds is equal to " + remainingSeconds);
+        }
+        else if (hours < 1 && remainingSeconds < 1) {
+            System.out.println(enteredSeconds + " seconds is equal to " + minutes + " minutes.");
+        }
+        else if (minutes < 1 && remainingSeconds < 1) {
+            System.out.println(enteredSeconds + " seconds is equal to " + hours + " hours.");
+        }
+        else if (hours < 1) {
+            System.out.println(enteredSeconds + " seconds is equal to " + minutes + " minutes, and " + remainingSeconds + " seconds.");
+        }
+        else if (minutes < 1) {
+            System.out.println(enteredSeconds + " seconds is equal to " + hours + " hours, and "  + remainingSeconds + " seconds.");
+        }
+        else if (remainingSeconds < 1) {
+            System.out.println(enteredSeconds + " seconds is equal to " + hours + " hours, and " + minutes + " minutes.");
+        }
         
-        System.out.println(enteredSeconds + " seconds is equal to " + hours + " hours, " + minutes + " minutes, and " + remainingSeconds + " seconds.");
-    }
+    }   
 }
