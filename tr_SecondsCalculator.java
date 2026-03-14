97a8301fea049902f81412a1a9476b1b11929cda
BurakHINGE <burakmentese16@gmail.com>
2026-03-14 03:15:59 +0300

    Update tr_SecondsCalculator.java
    Zero situations have been fixed.

--- tr_SecondsCalculator.java
+++ tr_SecondsCalculator.java
@@ -4,20 +4,34 @@ public class tr_SecondsCalculator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Saniye değeri giriniz: ");
-        int girilenSaniye = input.nextInt();
-    
-        int saat = girilenSaniye / 3600;
-        int saattenKalanSaniye = girilenSaniye % 3600;
-        int dakika = saattenKalanSaniye / 60;
-        int dakikadanKalanSaniye = saattenKalanSaniye % 60;
-        int kalanSaniye = dakikadanKalanSaniye;
+        while (true) {
+
+            System.out.print("Saniye değeri giriniz: ");
+            int girilenSaniye = input.nextInt();
         
-        
+            int saat = girilenSaniye / 3600;
+            int saattenKalanSaniye = girilenSaniye % 3600;
+            int dakika = saattenKalanSaniye / 60;
+            int dakikadanKalanSaniye = saattenKalanSaniye % 60;
+            int kalanSaniye = dakikadanKalanSaniye;
 
-        System.out.println(girilenSaniye + " saniye " + saat + " saat " + dakika + " dakika " + kalanSaniye + " saniye ediyor.");
+            System.out.print(girilenSaniye + " saniye: ");
+            
+            if (saat > 0) {
+                System.out.print(saat + " saat ");
+            }
+            if (dakika > 0) {
+                System.out.print(dakika + " dakika "); 
+            }
+            if (kalanSaniye > 0) {
+                System.out.print(kalanSaniye + " saniye ");
+            }
+            if (girilenSaniye == 0) {
+                System.out.print("0 saniye ");
+            }
 
-
-
-    }
+            System.out.print("ediyor.");
+            System.out.println();
+        }
+    }    
 }
